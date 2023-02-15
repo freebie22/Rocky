56e3a38916fa17c11290d77ad8e113942dca19e9
freebie22 <bojkov755@gmail.com>
2023-02-15 19:48:34 +0200

    Add project files.

---
+++ Rocky/Controllers/ApplicationTypeController.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
+using System.Data;
+
+namespace Rocky.Controllers
+{
+    [Authorize(Roles = WC.AdminRole)]
+    public class ApplicationTypeController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ApplicationTypeController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<ApplicationType> objList = _db.ApplicationType;
+            return View(objList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult>Create(ApplicationType obj)
+        {
+            if(ModelState.IsValid)
+            {
+                await Task.Run(() => {
+                    _db.AddAsync(obj);
+                    _db.SaveChangesAsync();
+                    
+                });
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id ==0) 
+            {
+                return NotFound();
+            }
+
+            var obj = _db.ApplicationType.Find(id);
+
+            if(obj == null) {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(ApplicationType obj)
+        {
+            if (ModelState.IsValid)
+            {
+                await Task.Run(() => {
+                    _db.Update(obj);
+                    _db.SaveChanges(); 
+                });
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var obj = _db.ApplicationType.Find(id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.ApplicationType.Find(id);
+            if (obj == null) {
+                return NotFound();
+            }
+
+            _db.ApplicationType.Remove(obj);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+
+        }
+
+    }
+}
---
+++ Rocky/Migrations/20230201172418_addAplicationType.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Rocky.Migrations
+{
+    /// <inheritdoc />
+    public partial class addAplicationType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            
+
+            migrationBuilder.AddColumn<int>(
+                name: "ApplicationTypeId",
+                table: "Product",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_ApplicationTypeId",
+                table: "Product",
+                column: "ApplicationTypeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Product_ApplicationType_ApplicationTypeId",
+                table: "Product",
+                column: "ApplicationTypeId",
+                principalTable: "ApplicationType",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Product_ApplicationType_ApplicationTypeId",
+                table: "Product");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Product_ApplicationTypeId",
+                table: "Product");
+
+            migrationBuilder.DropColumn(
+                name: "ApplicationTypeId",
+                table: "Product");
+
+            migrationBuilder.CreateTable(
+                name: "TestModel",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TestModel", x => x.Id);
+                });
+        }
+    }
+}
