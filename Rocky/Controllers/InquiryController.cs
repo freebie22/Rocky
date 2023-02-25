971b1869203e52c928add485f1e7dcc5581fb21a
freebie22 <bojkov755@gmail.com>
2023-02-25 11:40:13 +0200

    Section 5 - Added Inquiry Details, Convert To Cart and Delete Inquiry

--- Rocky/Controllers/InquiryController.cs
+++ Rocky/Controllers/InquiryController.cs
@@ -3,16 +3,20 @@ using Microsoft.AspNetCore.Mvc;
 using Rocky_DataAccess;
 using Rocky_DataAccess.Repository.IRepository;
 using Rocky_Models;
+using Rocky_Models.ViewModels;
 using Rocky_Utility;
 using System.Data;
 
 namespace Rocky.Controllers
 {
-    
+    [Authorize(WC.AdminRole)]
     public class InquiryController : Controller
     {
         private readonly IInquiryDetailRepository _inqDRepo;
         private readonly IInquiryHeaderRepository _inqHRepo;
+
+        [BindProperty]
+        public InquiryVM InquiryVM { get; set; }
 
         public InquiryController(IInquiryDetailRepository inqDRepo, IInquiryHeaderRepository inqHRepo)
         {
@@ -24,5 +28,56 @@ namespace Rocky.Controllers
         {
             return View();
         }
+
+        public IActionResult Details(int id)
+        {
+            InquiryVM = new InquiryVM()
+            {
+                InquiryHeader = _inqHRepo.FirstOrDefault(u => u.Id == id),
+                InquiryDetail = _inqDRepo.GetAll(u => u.InquiryHeaderId == id, includeProperties: "Product")
+            };
+            return View(InquiryVM);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Details()
+        {
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+            InquiryVM.InquiryDetail = _inqDRepo.GetAll(u=>u.InquiryHeaderId==InquiryVM.InquiryHeader.Id);
+
+            foreach(var detail in InquiryVM.InquiryDetail)
+            {
+                ShoppingCart shoppingCart = new ShoppingCart()
+                {
+                    ProductId = detail.ProductId,
+                };
+                shoppingCartList.Add(shoppingCart);
+            }
+            HttpContext.Session.Clear();
+            HttpContext.Session.Set(WC.SessionCart, shoppingCartList);
+            HttpContext.Session.Set(WC.SessionInquiryId, InquiryVM.InquiryHeader.Id);
+            return RedirectToAction("Index", "Cart");
+        }
+
+        [HttpPost]
+        public IActionResult Delete()
+        {
+            InquiryHeader inquiryHeader = _inqHRepo.FirstOrDefault(u=>u.Id == InquiryVM.InquiryHeader.Id);
+            IEnumerable<InquiryDetail> inquiryDetails = _inqDRepo.GetAll(u=>u.InquiryHeaderId == InquiryVM.InquiryHeader.Id);
+
+            _inqDRepo.RemoveRange(inquiryDetails);
+            _inqHRepo.Remove(inquiryHeader);
+            _inqHRepo.Save();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetInquiryList() 
+        {
+            return Json(new { data = _inqHRepo.GetAll() });
+        }
+        #endregion
     }
 }
